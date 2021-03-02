0b32f64217cf631ea471de489073355ee82855ce
Ivan Olmo <ivan.olmo@gmail.com>
2021-03-01 19:26:47 -0600

    Update api call. Add correct url and formatting to call the desired rover data

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,28 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 // your API calls
-
-// example API call
-app.get('/apod', async (req, res) => {
+app.get('/getRoverData', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
+        let rover = req.get('rover');
+        console.log(`calling api for data on ${rover}`);
+        let roverImages = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=100&api_key=${process.env.API_KEY}`)
+            .then(res => res.json());
+        res.send({ roverImages });
     } catch (err) {
         console.log('error:', err);
     }
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
