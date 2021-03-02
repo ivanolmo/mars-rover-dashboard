fcf8a700a0621d53da4b3b9c82c002d264201f12
Ivan Olmo <ivan.olmo@gmail.com>
2021-03-02 11:11:59 -0600

    Add new api call to retrieve the requested rovers mission manifest

--- src/server/index.js
+++ src/server/index.js
@@ -13,15 +13,29 @@ app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, '../public')));
 
 // your API calls
-app.get('/getRoverData', async (req, res) => {
+// call API to retrieve photos
+app.get('/getRoverPhotos', async (req, res) => {
     try {
         let rover = req.get('rover');
-        console.log(`calling api for data on ${rover}`);
+        console.log(`calling api for photos on ${rover}`);
         // use a ternary operator to change the Sol date based on which rover is requested. Perseverance just landed so as of today, Sol 9 is the latest date with available photos
         // only request one page of photos to speed up response, since some of the rovers can return nearly 500 photos in a single response
         let roverImages = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${rover == 'perseverance' ? 9 : 100}&page=1&api_key=${process.env.API_KEY}`)
             .then(res => res.json());
         res.send({ roverImages });
+    } catch (err) {
+        console.log('error:', err);
+    }
+});
+
+// call API to retrieve mission manifest
+app.get('/getRoverData', async (req, res) => {
+    try {
+        let rover = req.get('rover');
+        console.log(`calling api for data on ${rover}`);
+        let roverData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}/?api_key=${process.env.API_KEY}`)
+            .then(res => res.json());
+        res.send({ roverData });
     } catch (err) {
         console.log('error:', err);
     }
